89a8bdce695834df2e6d3d38618c6c45494a9d3b
Iqra Fatima <121562972+Iqra-F@users.noreply.github.com>
2025-08-28 00:04:41 +0500

    Update chart.tsx 2

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -73,7 +73,7 @@ ChartContainer.displayName = "Chart"
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
-    ([_, cfg]) => cfg.theme || cfg.color
+    ([_, config]) => config.theme || config.color
   )
 
   if (!colorConfig.length) return null
